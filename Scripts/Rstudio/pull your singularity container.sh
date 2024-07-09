5a4fd31fd1fe956012ebf380a62416ca56e6461a
Ahmed Hassan <37757151+Ahmedalaraby20@users.noreply.github.com>
2024-07-09 11:37:49 +0200

    Update pull your singularity container.sh

--- Scripts/Rstudio/pull your singularity container.sh
+++ Scripts/Rstudio/pull your singularity container.sh
@@ -1,15 +1,21 @@
-#Before start using Rstudio you should download a container that has both R and Rstudio installed 
-#There are plenty of options but if you want to work with Seurat or packages installed from Bioconductor I would strongly suggest using the Bioconductor container, 
-#There is a lengthy explanation/tutorial which can be found at https://hub.docker.com/r/bioconductor/bioconductor_docker 
-#In the HPC we are not allowed to use Docker for privacy concerns instead we use singularity (an alternative to Docker)
-#use the next lines to (download) pull whichever container you want to use #in here I am pulling the latest version of the Bioconductor container
-#Login to the HPC (see instructions to use HPC)
-#make sure you are in the project directory 
-#cd /project/your-ag-name/ #in my case /project/ag-georgiev/
-#make a new directory called Rstudio 
+```bash
+# Before starting with Rstudio, download a container that has both R and Rstudio installed.
+# There are plenty of options, but if you want to work with Seurat or packages installed from Bioconductor, I would strongly suggest using the Bioconductor container.
+# For a detailed tutorial, visit [Bioconductor Docker Hub](https://hub.docker.com/r/bioconductor/bioconductor_docker).
+
+# In the HPC, Docker cannot be used due to privacy concerns; instead, we use Singularity, an alternative to Docker.
+
+# Use the following lines to download/pull whichever container you want to use. Here, I am pulling the latest version of the Bioconductor container:
+
+# Login to the HPC (refer to instructions for using HPC).
+# Make sure you are in the project directory:
+cd /project/your-ag-name/   # Replace 'your-ag-name' with your specific project name, e.g., /project/ag-georgiev/
+
+# Create a new directory called 'Rstudio':
 mkdir Rstudio
-#go into the new directory
+
+# Navigate into the new directory:
 cd Rstudio
-#pull your container in my case I will get the Bioconductor container and save it as name.sif
+
+# Pull your container. In this case, I will get the Bioconductor container and save it as 'bioconductor.sif':
 singularity pull bioconductor.sif docker://bioconductor/bioconductor_docker
-#Thats it 
